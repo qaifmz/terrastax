41727a49f6576170b68bc35d4c0b3aa43c2b3b2c
qaifmz <qaifmz@gmail.com>
2020-06-04 21:28:40 -0700

    update variables

--- variables.tf
+++ variables.tf
@@ -1,3 +1,6 @@
+variable "region" {
+  default = "us-west-2"
+}
 
 variable "all_enabled" {
   description = "Bool to enable all services"
@@ -5,33 +8,14 @@ variable "all_enabled" {
   default     = true
 }
 
-
 variable "prometheus_enabled" {
   description = "Bool to enable prometheus"
   type        = bool
   default     = true
 }
 
-# variable "consul_enabled" {
-#   description = "Bool to enable consul"
-#   type        = bool
-#   default     = true
-# }
-
-# variable "elasticsearch_enabled" {
-#   description = "Bool to enable elasticsearch"
-#   type        = bool
-#   default     = true
-# }
-
 variable "nginx_ingress_enabled" {
   description = "Bool to enable nginx ingress"
   type        = bool
   default     = true
 }
-
-# variable "external_dns_enabled" {
-#   description = "Bool to enable external DNS controller"
-#   type        = bool
-#   default     = true
-# }
