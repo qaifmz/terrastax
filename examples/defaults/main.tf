62a9b3a2531a72fa153f88b0b49a7cc92ac0f969
rob <robcannonxyz@gmail.com>
2020-06-05 17:35:25 -0700

    adding example fixture

---
+++ examples/defaults/main.tf
@@ -0,0 +1,33 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "1.10"
+}
+
+resource "random_pet" "this" {
+  length = 2
+}
+
+module "eks" {
+  source             = "../.."
+  prometheus_enabled = true
+  region             = var.aws_region
+}
