faa28b6c861eb060bc93eaaf344a849bd61b812c
qaifmz <qaifmz@gmail.com>
2020-06-08 17:32:41 -0700

    add eks outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,37 @@
+##############
+# EKS ouptuts
+##############
 output "cluster_id" {
-  value = module.deploy-eks-clusters.cluster_id
+  description = "The name/id of the EKS cluster."
+  value       = module.deploy-eks-clusters.cluster_id
+}
+
+output "cluster_arn" {
+  description = "The Amazon Resource Name (ARN) of the cluster."
+  value       = module.deploy-eks-clusters.cluster_arn
+}
+
+output "cluster_certificate_authority_data" {
+  description = "Nested attribute containing certificate-authority-data for your cluster. This is the base64 encoded certificate data required to communicate with your cluster."
+  value       = module.deploy-eks-clusters.cluster_certificate_authority_data
+}
+
+output "cluster_endpoint" {
+  description = "The endpoint for your EKS Kubernetes API."
+  value       = module.deploy-eks-clusters.cluster_endpoint
+}
+
+output "cluster_version" {
+  description = "The Kubernetes server version for the EKS cluster."
+  value       = module.deploy-eks-clusters.cluster_version
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ID attached to the EKS cluster."
+  value       = module.deploy-eks-clusters.cluster_security_group_id
+}
+
+output "config_map_aws_auth" {
+  description = "A kubernetes configuration to authenticate to this EKS cluster."
+  value       = module.deploy-eks-clusters.config_map_aws_auth
 }
