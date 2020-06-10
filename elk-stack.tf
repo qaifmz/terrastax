c6dc2fd24bc8142ae457a52c0d676e81c12c01c7
qaifmz <qaifmz@gmail.com>
2020-06-09 22:23:43 -0700

    fix elk stack helm chart

--- elk-stack.tf
+++ elk-stack.tf
@@ -1,11 +1,14 @@
+data "helm_repository" "stable" {
+  name = "stable"
+  url  = "https://kubernetes-charts.storage.googleapis.com"
+}
+
 resource "helm_release" "elk-stack" {
   count            = var.elk-stack_enabled ? 1 : 0
   name             = "elk-stack"
-  chart            = "elastic-stack"
-  repository       = "stable"
+  chart            = "stable/elastic-stack"
+  repository       = data.helm_repository.stable.metadata[0].name
   create_namespace = "true"
   namespace        = "elk-stack"
-  wait             = true
-  force_update     = true
   values           = [file("${path.module}/elk-stack.yaml")]
 }
