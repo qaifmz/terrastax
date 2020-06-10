67aafa372b29c452d2ae3b0e73be265747edb4fe
qaifmz <qaifmz@gmail.com>
2020-06-10 15:09:39 -0700

    adding repos

--- main.tf
+++ main.tf
@@ -7,9 +7,26 @@ provider "aws" {
   region = var.aws_region
 }
 
+data "helm_repository" "stable" {
+  name = "stable"
+  url  = "https://kubernetes-charts.storage.googleapis.com"
+}
+
+data "helm_repository" "bitnami" {
+  name = "bitnami"
+  url  = "https://charts.bitnami.com/bitnami"
+}
+
+data "helm_repository" "jetstack" {
+  name = "jetstack"
+  url  = "https://charts.jetstack.io"
+}
+
+data "helm_repository" "elastic" {
+  name = "elastic"
+  url  = "https://helm.elastic.co"
+}
+
 locals {
-  prometheus_enabled    = var.all_enabled ? true : var.prometheus_enabled
-  prometheus-operator_enabled    = var.all_enabled ? true : var.prometheus-operator_enabled
-  nginx_ingress_enabled = var.all_enabled ? true : var.nginx_ingress_enabled
-  grafana_enabled       = var.all_enabled ? true : var.grafana_enabled
+  prometheus-operator_enabled     = var.all_enabled ? true : var.prometheus-operator_enabled
 }
