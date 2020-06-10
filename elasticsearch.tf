750e37bf694a61a6a31133bae1e7d972397df3a5
qaifmz <qaifmz@gmail.com>
2020-06-09 17:27:40 -0700

    update elastic fix

--- elasticsearch.tf
+++ elasticsearch.tf
@@ -9,6 +9,7 @@ resource "helm_release" "elasticsearch" {
   name       = "elasticsearch"
   chart      = "stable/elasticsearch"
   repository = data.helm_repository.stable.metadata[0].name
+  create_namespace = "true"
   namespace  = "elk-stack"
   values     = [file("${path.module}/elasticsearch.yaml")]
 }
