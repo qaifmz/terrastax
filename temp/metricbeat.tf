358f4786e314f67fe7bc6d3d5e03afc4a5e3b6d6
qaifmz <qaifmz@gmail.com>
2020-06-11 16:10:59 -0700

    add metricbeat helm chart

---
+++ temp/metricbeat.tf
@@ -0,0 +1,11 @@
+resource "helm_release" "metricbeat" {
+  count      = var.metricbeat_enabled ? 1 : 0
+  name       = "metricbeat"
+  chart      = "stable/metricbeat"
+  repository = data.helm_repository.stable.metadata[0].name
+  namespace  = "elk-stack"
+  values     = [file("${path.module}/metricbeat.yaml")]
+  wait             = true
+  force_update     = true
+  # timeout          = 900
+}
