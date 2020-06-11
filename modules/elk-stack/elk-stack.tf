6f1953a58ec9e09c61e80316538e9c83cb7020d4
qaifmz <qaifmz@gmail.com>
2020-06-10 18:14:39 -0700

    update chart

--- modules/elk-stack/elk-stack.tf
+++
@@ -1,9 +0,0 @@
-resource "helm_release" "elk-stack" {
-  count            = var.elk-stack_enabled ? 1 : 0
-  name             = "elk-stack"
-  chart            = "stable/elastic-stack"
-  repository       = data.helm_repository.stable.metadata[0].name
-  create_namespace = "true"
-  namespace        = "elk-stack"
-  values           = [file("${path.module}/elk-stack.yaml")]
-}
