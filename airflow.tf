8f808be8c54792b112edf10a6b0e25d1669f0d9e
qaifmz <qaifmz@gmail.com>
2020-06-16 18:55:29 -0700

    change namespace

--- airflow.tf
+++ airflow.tf
@@ -4,25 +4,9 @@ resource "helm_release" "airflow" {
   chart            = "stable/airflow"
   repository       = data.helm_repository.stable.metadata[0].name
   create_namespace = "true"
-  namespace        = "logging"
+  namespace        = "monitoring"
   values           = [file("${path.module}/airflow.yaml")]
   wait             = true
   force_update     = true
-  # timeout          = 900
-  # set {
-  #   name  = "airflow.cloneDagFilesFromGit.enabled"
-  #   value = "true"
-  # }
-  # set {
-  #   name  = "airflow.cloneDagFilesFromGit.repository"
-  #   value = "https://github.com/bitnami/tutorials/tree/master/airflow-dag-examples"
-  # }
-  # set {
-  #   name  = "airflow.cloneDagFilesFromGit.branch"
-  #   value = "master"
-  # }
-  # set {
-  #   name  = "airflow.baseURL"
-  #   value = "http://127.0.0.1:8080"
-  # }
 }
+
