687cc9f42938ff180573aed6ea8f1e7acefb6d9e
Qaif Shaikh <qaifmz@gmail.com>
2020-06-27 18:38:46 -0700

    adding_comments

--- examples/defaults/main.tf
+++ examples/defaults/main.tf
@@ -1,3 +1,7 @@
+##############
+# Provider
+##############
+
 variable "id" {
   description = "The id of the resources"
   type        = string
@@ -14,6 +18,9 @@ provider "aws" {
   region = var.aws_region
 }
 
+##############
+# Random
+##############
 resource "random_pet" "this" {
   length = 2
 }
@@ -23,10 +30,9 @@ module "defaults" {
   id     = random_pet.this.id
 }
 
-module "eks" {
-  source = "github.com/qaifmz/terraform-aws-eks-base"
-  id     = var.id
-}
+##############
+# Base
+##############
 
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_id
@@ -43,32 +49,3 @@ provider "kubernetes" {
   load_config_file       = false
   version                = "1.10"
 }
-
-
-module "nginx_ingress" {
-  source = "./modules/nginx_ingress"
-}
-
-module "prometheus-operator" {
-  source = "./modules/prometheus-operator"
-}
-
-module "elk-stack" {
-  source = "./modules/elk-stack"
-}
-
-module "fluentd" {
-  source = "./modules/fluentd"
-}
-
-module "airflow" {
-  source = "./modules/airflow"
-}
-
-# locals {
-#   nginx_ingress_enabled       = var.all_enabled ? true : var.nginx_ingress_enabled
-#   prometheus-operator_enabled = var.all_enabled ? true : var.prometheus-operator_enabled
-#   fluentd_enabled             = var.all_enabled ? true : var.fluentd_enabled
-#   elk-stack_enabled           = var.all_enabled ? true : var.elk-stack_enabled
-#   airflow_enabled             = var.all_enabled ? true : var.airflow_enabled
-# }
