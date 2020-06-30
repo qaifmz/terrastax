86c7c5a2cc4f9d62804579ccae405b226ce7ee03
Qaif Shaikh <qaifmz@gmail.com>
2020-06-29 23:42:17 -0700

    add condition

--- elk-stack.tf
+++ elk-stack.tf
@@ -5,6 +5,7 @@ data "template_file" "elk-stack" {
 
 # Render template
 resource "null_resource" "create-elk-stack" {
+  count = var.elk-stack_enabled ? 1 : 0
   triggers = {
     manifest_sha1 = "${sha1("${data.template_file.elk-stack.rendered}")}"
   }
@@ -16,6 +17,7 @@ resource "null_resource" "create-elk-stack" {
 
 # Run Command on Destroy
 resource "null_resource" "destroy-elk-stack" {
+  count = var.elk-stack_enabled ? 1 : 0
   provisioner "local-exec" {
     when    = "destroy"
     command = "kubectl delete -f -<<EOF\n${data.template_file.elk-stack.rendered}\nEOF"
