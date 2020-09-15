e47fc12c07f41e3fed8cf91366da231f99205c80
Qaif Amaan Shaikh <44881065+qaifmz@users.noreply.github.com>
2020-09-14 22:46:58 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -12,19 +12,12 @@ output "prometheus-operator" {
   value       = var.prometheus-operator_enabled
 }
 
-output "fluentd" {
-  description = "Fluentd installed"
-  value       = var.fluentd_enabled
-}
-
 output "elk-stack" {
   description = "ELK Stack installed"
   value       = var.elk-stack_enabled
-  depends_on  = ["module.fluentd"]
 }
 
 output "airflow" {
   description = "Airflow installed"
   value       = var.airflow_enabled
-  depends_on  = ["module.prometheus-operator"]
 }
